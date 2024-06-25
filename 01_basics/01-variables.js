e1aa523cb475e10fcebd1c5c2fa961d0a1238721
Satyam876 <satyamkumarbokaro123@gmail.com>
2024-06-25 11:50:28 +0530

    Second commit - Variables & DataTypes

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,19 @@
+// console.log("Hello again ! Satyam")
+
+const accountId = 122345;
+let accountEmail = "satyamxx@gmail.com"
+var accountPassword = "Satyam#xxxx"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 10;
+// console.log(accountId);
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+accountEmail = "kumar19999@gmail.com"
+accountPassword = "kumar21@xxx"
+accountCity = "Bangalore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
